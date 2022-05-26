0e2139f82148f224a902d98441ac26e4a170ab20
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-05-26 18:11:11 +0530

    Version 1.4.1 : Added Data Validator using express-validator, Added inotebook db.

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 // Connection String of MongoDB Compass:
 // const mongoURI = `mongodb://localhost:27017`;
-const mongoURI = `mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
+const mongoURI = `mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
 // Connecting to MongoDB from this function.
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => { console.log("Connected to Mongo Successfully...") })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 // Create a User using:POST "api/auth/". Dous not require Authentication.
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-    const user = User(req.body);
-    // Saving data into MongoDB
-    user.save();
+router.post("/", [
+    body('name', "Please enter a valid Name...").isLength({ min: 3 }),
+    body('email', "Please enter valid email in format 'name@example.com'...").isEmail(),
+    body('password', "Password must be atleast 8 Characters...").isLength({ min: 8 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+    }).then(user => res.json(user)).catch(err => {
+        console.log(err);
+        res.json({ error: "Please enter a unique value for email..." });
+    });
 });
 
 module.exports = router;
