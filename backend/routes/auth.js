25207395f27379e8790aca26e706bbbe64ee0b96
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-05-27 01:45:52 +0530

    Version 1.7.1 : Created Login End Point and Sending Auth token to the user.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -46,8 +46,41 @@ router.post("/createuser", [
 
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("Some Error Occured");
+        res.status(500).send("Internal Server Occured");
     }
 });
 
+// Authenticate a User using:POST "api/auth/login". No login required.
+router.post("/login", [
+    body('email', "Please enter valid email in format 'name@example.com'...").isEmail(),
+    body('password', "Password cannot be blank").exists()
+], async (req, res) => {
+    // If there are errors return bad request and errors.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    const { email, password } = req.body;
+    try {
+        // Checking user from mail from DB
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please try to login with correct credentials..." })
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Please try to login with correct credentials..." })
+        }
+        // Taking id from creating user.
+        const data = {
+            user: { id: user.id }
+        }
+        // Generating authentication token here
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.send({ authToken });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Occured");
+    }
+})
 module.exports = router;
